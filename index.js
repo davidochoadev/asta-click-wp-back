24d795a443ae99880c8bf7853af5b66eca0bf78a
davideochoaa <davideochoaa@gmail.com>
2023-06-26 22:18:36 +0200

    ✅ Feat: refactoring for vercel

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-import express from "express";
-import cors from 'cors';
-import corsOptions from "./config/corsOptions.js";
-import { performSearch } from "./controllers/searchController.js";
+const express = require("express");
+const cors = require('cors');
+const corsOptions = require("./config/corsOptions.js");
+const { performSearch } = require("./controllers/searchController.js");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
-    res.send("Benvenuto nel server Express!");
-  });
-
-  app.get("/search", performSearch);
-
-app.listen(process.env.PORT || 8000, ()=>{
-    console.log("Backend started")
+  res.send("Benvenuto nel server Express!");
 });
+
+app.get("/search", performSearch);
+
+module.exports = app;
