9f9c55c76e8ce2d3822bce8050c3ae20f7a7b195
davideochoaa <davideochoaa@gmail.com>
2023-06-26 22:58:13 +0200

    ✅ Feat: Created route to download export_piemonte

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from 'cors';
 import corsOptions from "./config/corsOptions.js";
 import { performSearch } from "./controllers/searchController.js";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express()
 app.use(express.json())
@@ -12,6 +15,25 @@ app.get("/", (req, res) => {
     res.send("Benvenuto nel server Express!");
   });
 
+  app.get("/Data/export_piemonte.csv", (req, res) => {
+    const filePath = path.join(process.cwd(), "data", "export_piemonte.csv");
+  
+    // Read the file contents
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        res.status(500).send("Internal Server Error");
+      } else {
+        // Set the appropriate headers for CSV response
+        res.setHeader("Content-Type", "text/csv");
+        res.setHeader("Content-Disposition", "attachment; filename=export_piemonte.csv");
+  
+        // Send the file contents as the response
+        res.send(data);
+      }
+    }).then(res.status(200).send("Successful Download!"));
+  });
+  
   app.get("/search", performSearch);
 
 app.listen(process.env.PORT || 8000, ()=>{
