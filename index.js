55acdaec6099e0ae6d255695609cacf501978e54
davideochoaa <davideochoaa@gmail.com>
2023-06-26 22:22:07 +0200

    ✅ Feat: fixing vercel serverless function

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const cors = require('cors');
-const corsOptions = require("./config/corsOptions.js");
-const { performSearch } = require("./controllers/searchController.js");
+import express from "express";
+import cors from 'cors';
+import corsOptions from "./config/corsOptions.js";
+import { performSearch } from "./controllers/searchController.js";
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 app.get("/", (req, res) => {
-  res.send("Benvenuto nel server Express!");
+    res.send("Benvenuto nel server Express!");
+  });
+
+  app.get("/search", performSearch);
+
+app.listen(process.env.PORT || 8000, ()=>{
+    console.log("Backend started")
 });
-
-app.get("/search", performSearch);
-
-module.exports = app;
